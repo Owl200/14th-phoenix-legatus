3de52601a5925e6d3703deadaf4356a1fd8dbc5f
Owl200 <alromaru@hotmail.com>
2022-06-09 04:51:29 -0500

    added animated floating menu for mobile version

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,28 +1,83 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import { motion } from 'framer-motion'
+import { motion, useCycle } from 'framer-motion'
 import './Navbar.css'
+
+//variants for the nav background
+const sidebar = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at calc(100% - 38px) calc(100% - 47px))`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2
+    }
+  }),
+  closed: {
+    clipPath: "circle(30px at calc(100% - 38px) calc(100% - 47px))",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40
+    }
+  }
+};
+
+//variants for use on the Navlink animations
+const variants = {
+  open: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 }
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 }
+  }
+};
+
+//variants for the li on the Navlinks
+const liVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 }
+    }
+  },
+  closed: {
+    y: 1000,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 }
+    }
+  }
+};
+
+//creates a Path component to be used on the SVG
+const Path = props => (
+  <motion.path
+    fill="transparent"
+    strokeWidth="3"
+    stroke="hsl(0, 0%, 18%)"
+    strokeLinecap="round"
+    {...props}
+  />
+);
 
 const Navbar = () => {
   const [windowSize, setWindowSize] = useState(window.innerWidth)
-  const [navIsVisible, setNavIsVisible] = useState(false)
+  //const [navIsVisible, setNavIsVisible] = useState(false)
+  const [isOpen, toggleOpen] = useCycle(false, true)
 
+  //gets the current windos size to check on what type of nav will be rendered
   useEffect( _ => {
     window.addEventListener('resize', _ => setWindowSize(window.innerWidth))
   }, [windowSize])
 
-  const Path = props => (
-    <motion.path
-      fill="transparent"
-      strokeWidth="3"
-      stroke="hsl(0, 0%, 18%)"
-      strokeLinecap="round"
-      {...props}
-    />
-  );
-
-  const handleClick = () => setNavIsVisible(!navIsVisible)
+  // handles the state for the animation on the button
+  const handleClick = () => {
+    toggleOpen()
+  }
 
   return (
     <>
@@ -36,42 +91,34 @@ const Navbar = () => {
             <li><NavLink to='/contact-us' className='nav__list-item' activeClassName='nav__list-item--active'>Contactanos</NavLink></li>
         </ul>
     </nav> :
-    !navIsVisible ?
-    <button id='nav__button' onClick={handleClick}>
-       <svg width="23" height="23" viewBox="0 0 23 23">
-        <Path
-          variants={{
-            closed: { d: "M 2 2.5 L 20 2.5" },
-            open: { d: "M 3 16.5 L 17 2.5" }
-          }}
-        />
-        <Path
-          d="M 2 9.423 L 20 9.423"
-          variants={{
-            closed: { opacity: 1 },
-            open: { opacity: 0 }
-          }}
-          transition={{ duration: 0.1 }}
-        />
-        <Path
-          variants={{
-            closed: { d: "M 2 16.346 L 20 16.346" },
-            open: { d: "M 3 2.5 L 17 16.346" }
-          }}
-        />
-      </svg>
-    </button> :
     <>
-      <button id='nav__list-mini-button--clicked' onClick={handleClick}>+</button>
-      <nav id='nav_mini'>
-        <ul id='nav__list-mini'>
-          <li><NavLink to='/' activeClassName='nav__list-mini--active' onClick={handleClick}>Inicio</NavLink></li>
-          <li><NavLink to='/about-us' activeClassName='nav__list-mini--active' onClick={handleClick}>Conocenos</NavLink></li>
-          <li><NavLink to='/house' activeClassName='nav__list-mini--active' onClick={handleClick}>Casa</NavLink></li>
-          <li><NavLink to='/ranks' activeClassName='nav__list-mini--active' onClick={handleClick}>Rangos</NavLink></li>
-          <li><NavLink to='/contact-us' activeClassName='nav__list-mini--active' onClick={handleClick}>Contactanos</NavLink></li>
-        </ul>
-      </nav>
+      <motion.nav id='nav_mini' initial={false} animate={isOpen ? "open" : "closed"} >
+        <motion.button id='nav__button' onClick={handleClick} >
+          <svg width='23' height='23' viewbox='0 0 23 23' >
+            <Path variants={ {closed: { d: "M 2 2.5 L 20 2.5" }, open: { d: "M 3 16.5 L 17 2.5" }} }/>
+            <Path d="M 2 9.423 L 20 9.423" variants={ { closed: { opacity: 1 }, open: { opacity: 0 }} } transition={{ duration: 0.1 }}/>
+            <Path variants={ {closed: { d: "M 2 16.346 L 20 16.346" }, open: { d: "M 3 2.5 L 17 16.346" }} }/>
+          </svg>  
+        </motion.button> 
+        <motion.div className='nav_mini-background' variants={sidebar}/>
+        <motion.ul id='nav__list-mini' variants={variants}>
+          <motion.li variants={liVariants}>
+            <NavLink to='/' activeClassName='nav__list-mini--active' onClick={handleClick}>Inicio</NavLink>
+          </motion.li>
+          <motion.li variants={liVariants}>
+            <NavLink to='/about-us' activeClassName='nav__list-mini--active' onClick={handleClick}>Conocenos</NavLink>
+          </motion.li>
+          <motion.li variants={liVariants}>
+            <NavLink to='/house' activeClassName='nav__list-mini--active' onClick={handleClick}>Casa</NavLink>
+          </motion.li>
+          <motion.li variants={liVariants}>
+            <NavLink to='/ranks' activeClassName='nav__list-mini--active' onClick={handleClick}>Rangos</NavLink>
+          </motion.li>
+          <motion.li variants={liVariants}>
+            <NavLink to='/contact-us' activeClassName='nav__list-mini--active' onClick={handleClick}>Contactanos</NavLink>
+          </motion.li>
+        </motion.ul>
+      </motion.nav>
     </>
   }
   </>
