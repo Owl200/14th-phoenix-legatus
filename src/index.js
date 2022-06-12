d20e9aac9d038170a8f1d2cc5c2d644ffa9c6c97
Owl200 <alromaru@hotmail.com>
2022-06-12 08:42:50 -0500

    fixed title on index.html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
+//import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'; // for use on Github pages
 import './index.css';
 import App from './App';
 import ScrollToTop from './ScrollToTop'
@@ -9,10 +10,12 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter>
       <ScrollToTop />
       <App />
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
 
